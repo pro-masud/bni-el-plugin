d8ad6075b1104dcc2cae074d515301021e50fdb5
pro-masud <promasudbd@gmail.com>
2025-09-24 18:33:36 +0600

    collabaration profile add successfully

--- bni-widgets.php
+++ bni-widgets.php
@@ -67,7 +67,7 @@ function show_related_profiles_two() {
                         <?php echo get_the_post_thumbnail($profile->ID); ?>
                     </div>
                     <div class="bni-content">
-                        <h3><a href="<?php echo get_permalink($profile->ID); ?>">K<?php echo get_the_title($profile->ID); ?></a></h3>
+                        <h3><a href="<?php echo get_permalink($profile->ID); ?>"><?php echo get_the_title($profile->ID); ?></a></h3>
                     </div>
                 </div>
             <?php } ?>
@@ -78,5 +78,97 @@ function show_related_profiles_two() {
 }
 
 add_shortcode('show_related_profiles_two', 'show_related_profiles_two');
+
+
+function show_related_single_profile() {
+    $profiles = get_field('member'); 
+    if( $profiles ) {
+       ?>
+
+        <div class="bni-team-member-wrapper bni-team-single-member">
+            <?php foreach( $profiles as $profile ) { ?>
+                <div class="bni-single-member">
+                    <div class="bni-author-img">
+                        <?php echo get_the_post_thumbnail($profile->ID); ?>
+                    </div>
+                    <div class="bni-content">
+                        <h3><a href="<?php echo get_permalink($profile->ID); ?>"><?php echo get_the_title($profile->ID); ?></a></h3>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+
+       <?php
+    }
+}
+
+add_shortcode('show_related_single_profile', 'show_related_single_profile');
+
+
+function collaboration_partner_collaboration_post() {
+    // Get relationship field (ACF) that lists case studies/posts
+    $collaboration_partner_collaboration = get_field('collaboration_partner_collaboration');
+
+    if (empty($collaboration_partner_collaboration)) {
+        return;
+    }
+    ?>
+    <section class="bni-case">
+        <?php
+        foreach ((array) $collaboration_partner_collaboration as $single_col) {
+            $single_col_id = is_object($single_col) ? $single_col->ID : (int) $single_col;
+
+            $collaborators_title = get_field('collaborators_title', $single_col_id);
+            $button_text = get_field('button_text', $single_col_id);
+
+            $permalink = get_permalink($single_col_id);
+            $title     = get_the_title($single_col_id);
+
+            $content   = get_post_field('post_content', $single_col_id);
+            $excerpt   = wp_trim_words( wp_strip_all_tags($content), 13, '…' );
+            ?>
+            <div class="bni-card">
+                <div class="bni-media">
+                    <?php  echo get_the_post_thumbnail(  $single_col_id, 'medium',  array('alt' => esc_attr($title))  ); ?>
+                </div>
+                <div class="bni-content">
+                    <h3 class="bni-title">
+                        <?php echo esc_html($title); ?>
+                    </h3>
+                    <p class="bni-subtitle"><?php echo esc_html($excerpt); ?></p>
+                    <div class="bni-collab-group">
+                        <div class="bni-collab-title"><?php echo esc_html($collaborators_title); ?></div>
+
+                        <?php $members = get_field('member', $single_col_id); ?>
+                            <div class="bni-team-member-wrapper">
+                                <?php foreach ((array) $members as $member) :
+                                    $member_id    = is_object($member) ? $member->ID : (int) $member;
+                                    $member_title = get_the_title($member_id);
+                                    ?>
+                                    <div class="bni-single-member">
+                                        <div class="bni-author-img">
+                                            <?php echo get_the_post_thumbnail( $member_id,  'thumbnail',  array('alt' => esc_attr($member_title)) ); ?>
+                                        </div>
+                                        <div class="bni-content">
+                                            <a href="<?php echo esc_url(get_permalink($member_id)); ?>">
+                                                <?php echo esc_html($member_title); ?>
+                                            </a>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                    </div>
+                    <a href="<?php echo esc_url($permalink); ?>" class="bni-btn"><?php echo esc_html($button_text); ?></a>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </section>
+    <?php
+}
+
+
+add_shortcode('collaboration_partner_collaboration_post', 'collaboration_partner_collaboration_post');
 
 
